8e548239e3124535f8c024648763c49e854d5ff6
Shelsy Garcia <shelsyangelik@gmail.com>
2022-04-28 15:15:37 -0500

    añadimos plugin para trabajar con HTML dentro de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require ('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -20,4 +21,11 @@ module.exports = {
             }
         ]
     },
+    plugins:[
+        new HtmlWebPackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
 }
