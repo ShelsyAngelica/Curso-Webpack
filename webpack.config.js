44430f58c6f29a6bdac6652bb97c5c764b207189
Shelsy Garcia <shelsyangelik@gmail.com>
2022-04-29 11:33:36 -0500

    Añadimos loader de fuentes y agregamos la configuracion

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: 'main.js',
+        assetModuleFilename:'assets/images/[hash][ext][query]'
     },
     resolve: {
         extensions: ['.js']
@@ -31,6 +32,13 @@ module.exports = {
             {
                 test: /\.png/,
                 type:'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/fonts/[hash][ext]",
+                }
             }
         ]
     },
