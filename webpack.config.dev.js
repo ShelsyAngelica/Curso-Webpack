6baa7a492982272a9ac663714f81cfb7436ffb71
Shelsy Garcia <shelsyangelik@gmail.com>
2022-05-05 10:33:10 -0500

    añadio conf

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv =require('dotenv-webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/index.js',
